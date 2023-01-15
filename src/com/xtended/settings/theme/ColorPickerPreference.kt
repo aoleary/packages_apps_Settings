Subratam6296/Settings

Settings: Use colorkt-based dynamic theme engine for Monet [2/2]

src/com/arrow/settings/theme/ColorPickerPreference.kt
@@ -1,0 +1,38 @@
+/*
+ * Copyright (C) 2022
+ * SPDX-License-Identifier: Apache-2.0
+*/
+
+package com.arrow.settings.theme
+
+import android.content.Context
+import android.content.res.TypedArray
+import android.util.AttributeSet
+
+import androidx.preference.DialogPreference
+
+open class ColorPickerPreference @JvmOverloads constructor(
+    context: Context,
+    attrs: AttributeSet? = null,
+) : DialogPreference(context, attrs) {
+
+    var color: String? = null
+        private set
+
+    override protected fun onGetDefaultValue(a: TypedArray, index: Int): Any? {
+        return a.getString(index)
+    }
+
+    override protected fun onSetInitialValue(restoreValue: Boolean, defaultValue: Any?) {
+        val def = defaultValue as? String
+        color = if (restoreValue) getPersistedString(def) else def
+        setSummary(color)
+    }
+
+    fun setColor(color: String) {
+        if (!callChangeListener(color)) return
+        this.color = color
+        setSummary(color)
+        persistString(color)
+    }
+}

src/com/arrow/settings/theme/MonetChromaFactorPreferenceController.kt
@@ -1,0 +1,50 @@
+/*
+ * Copyright (C) 2022
+ * SPDX-License-Identifier: Apache-2.0
+*/
+
+package com.arrow.settings.theme
+
+import android.content.Context
+import android.os.UserHandle
+import android.provider.Settings
+
+import androidx.preference.Preference
+
+import com.android.settings.core.BasePreferenceController
+import com.arrow.support.preferences.CustomSeekBarPreference
+
+class MonetChromaFactorPreferenceController(
+    context: Context,
+    key: String,
+) : BasePreferenceController(context, key),
+    Preference.OnPreferenceChangeListener {
+
+    override fun getAvailabilityStatus(): Int = AVAILABLE
+
+    override fun updateState(preference: Preference) {
+        super.updateState(preference)
+        val chromaFactor = Settings.Secure.getFloatForUser(
+            mContext.contentResolver,
+            Settings.Secure.MONET_ENGINE_CHROMA_FACTOR,
+            CHROMA_DEFAULT,
+            UserHandle.USER_CURRENT
+        ) * 100
+        (preference as CustomSeekBarPreference).apply {
+            setValue(chromaFactor.toInt())
+        }
+    }
+
+    override fun onPreferenceChange(preference: Preference, newValue: Any): Boolean {
+        return Settings.Secure.putFloatForUser(
+            mContext.contentResolver,
+            Settings.Secure.MONET_ENGINE_CHROMA_FACTOR,
+            (newValue as Int) / 100f,
+            UserHandle.USER_CURRENT
+        )
+    }
+
+    companion object {
+        private const val CHROMA_DEFAULT = 1f
+    }
+}

src/com/arrow/settings/theme/MonetEngine.kt
@@ -1,0 +1,26 @@
+/*
+ * Copyright (C) 2022
+ * SPDX-License-Identifier: Apache-2.0
+*/
+
+package com.arrow.settings.theme
+
+import com.android.settings.R
+import com.android.settings.search.BaseSearchIndexProvider
+import com.android.settingslib.search.SearchIndexable
+import com.arrow.settings.theme.XDashboardFragment
+
+@SearchIndexable
+class MonetEngine : XDashboardFragment() {
+
+    override protected fun getPreferenceScreenResId() = R.xml.monet
+
+    override protected fun getLogTag() = TAG
+
+    companion object {
+        private const val TAG = "MonetEngine"
+
+        @JvmField
+        val SEARCH_INDEX_DATA_PROVIDER = BaseSearchIndexProvider(R.xml.monet)
+    }
+}

src/com/arrow/settings/theme/SecureSettingsStore.kt
@@ -1,0 +1,51 @@
+/*
+ * Copyright (C) 2022
+ * SPDX-License-Identifier: Apache-2.0
+*/
+
+package com.arrow.settings.theme
+
+import android.content.ContentResolver
+import android.provider.Settings
+
+import androidx.preference.PreferenceDataStore
+
+class SecureSettingsStore(
+    private val contentResolver: ContentResolver
+): PreferenceDataStore() {
+
+    override fun getBoolean(key: String, defValue: Boolean) =
+        getInt(key, if (defValue) 1 else 0) == 1
+
+    override fun getFloat(key: String, defValue: Float) =
+        Settings.Secure.getFloat(contentResolver, key, defValue)
+
+    override fun getInt(key: String, defValue: Int) =
+        Settings.Secure.getInt(contentResolver, key, defValue)
+
+    override fun getLong(key: String, defValue: Long) =
+        Settings.Secure.getLong(contentResolver, key, defValue)
+
+    override fun getString(key: String, defValue: String?): String? =
+        Settings.Secure.getString(contentResolver, key) ?: defValue
+
+    override fun putBoolean(key: String, value: Boolean) {
+        putInt(key, if (value) 1 else 0)
+    }
+
+    override fun putFloat(key: String, value: Float) {
+        Settings.Secure.putFloat(contentResolver, key, value)
+    }
+
+    override fun putInt(key: String, value: Int) {
+        Settings.Secure.putInt(contentResolver, key, value)
+    }
+
+    override fun putLong(key: String, value: Long) {
+        Settings.Secure.putLong(contentResolver, key, value)
+    }
+
+    override fun putString(key: String, value: String?) {
+        Settings.Secure.putString(contentResolver, key, value)
+    }
+}

src/com/arrow/settings/theme/SystemSettingColorPickerPreference.kt
@@ -1,0 +1,18 @@
+/*
+ * Copyright (C) 2022
+ * SPDX-License-Identifier: Apache-2.0
+*/
+
+package com.arrow.settings.theme
+
+import android.content.Context
+import android.util.AttributeSet
+
+public class SystemSettingColorPickerPreference @JvmOverloads constructor(
+    context: Context,
+    attrs: AttributeSet? = null,
+): ColorPickerPreference(context, attrs) {
+    init {
+        setPreferenceDataStore(SystemSettingsStore(context.contentResolver))
+    }
+}

src/com/arrow/settings/theme/SystemSettingsStore.kt
@@ -1,0 +1,51 @@
+/*
+ * Copyright (C) 2022
+ * SPDX-License-Identifier: Apache-2.0
+*/
+
+package com.arrow.settings.theme
+
+import android.content.ContentResolver
+import android.provider.Settings
+
+import androidx.preference.PreferenceDataStore
+
+class SystemSettingsStore(
+    private val contentResolver: ContentResolver
+): PreferenceDataStore() {
+
+    override fun  getBoolean(key: String, defValue: Boolean) =
+        getInt(key, if (defValue) 1 else 0) == 1
+
+    override fun  getFloat(key: String, defValue: Float) =
+        Settings.System.getFloat(contentResolver, key, defValue)
+
+    override fun  getInt(key: String, defValue: Int) =
+        Settings.System.getInt(contentResolver, key, defValue)
+
+    override fun  getLong(key: String, defValue: Long) =
+        Settings.System.getLong(contentResolver, key, defValue)
+
+    override fun  getString(key: String, defValue: String?): String? =
+        Settings.System.getString(contentResolver, key) ?: defValue
+
+    override fun  putBoolean(key: String, value: Boolean) {
+        putInt(key, if (value) 1 else 0)
+    }
+
+    override fun  putFloat(key: String, value: Float) {
+        Settings.System.putFloat(contentResolver, key, value)
+    }
+
+    override fun  putInt(key: String, value: Int) {
+        Settings.System.putInt(contentResolver, key, value)
+    }
+
+    override fun  putLong(key: String, value: Long) {
+        Settings.System.putLong(contentResolver, key, value)
+    }
+
+    override fun  putString(key: String, value: String?) {
+        Settings.System.putString(contentResolver, key, value)
+    }
+}

src/com/arrow/settings/theme/XDashboardFragment.kt
@@ -1,0 +1,34 @@
+/*
+ * Copyright (C) 2022
+ * SPDX-License-Identifier: Apache-2.0
+*/
+
+package com.arrow.settings.theme
+
+import androidx.preference.Preference
+
+import com.android.internal.logging.nano.MetricsProto
+import com.android.settings.dashboard.DashboardFragment
+import com.arrow.settings.theme.ColorPickerFragment
+import com.arrow.settings.theme.ColorPickerPreference
+
+abstract class XDashboardFragment : DashboardFragment() {
+
+    override fun getMetricsCategory(): Int = MetricsProto.MetricsEvent.ARROW
+
+    override fun onDisplayPreferenceDialog(preference: Preference) {
+        if (preference is ColorPickerPreference) {
+            ColorPickerFragment(preference.color).apply {
+                setOnConfirmListener {
+                    preference.setColor(it)
+                }
+            }.show(childFragmentManager, COLOR_PICKER_DIALOG_KEY)
+        } else {
+            super.onDisplayPreferenceDialog(preference)
+        }
+    }
+
+    companion object {
+        const val COLOR_PICKER_DIALOG_KEY = "color_picker_dialog"
+    }
+}

aoleary/packages_apps_Settings

src/com/xtended/settings/theme/ColorPickerPreference.kt
@@ -1,0 +1,38 @@
+/*
+ * Copyright (C) 2022
+ * SPDX-License-Identifier: Apache-2.0
+*/
+
+package com.xtended.settings.theme
+
+import android.content.Context
+import android.content.res.TypedArray
+import android.util.AttributeSet
+
+import androidx.preference.DialogPreference
+
+open class ColorPickerPreference @JvmOverloads constructor(
+    context: Context,
+    attrs: AttributeSet? = null,
+) : DialogPreference(context, attrs) {
+
+    var color: String? = null
+        private set
+
+    override protected fun onGetDefaultValue(a: TypedArray, index: Int): Any? {
+        return a.getString(index)
+    }
+
+    override protected fun onSetInitialValue(restoreValue: Boolean, defaultValue: Any?) {
+        val def = defaultValue as? String
+        color = if (restoreValue) getPersistedString(def) else def
+        setSummary(color)
+    }
+
+    fun setColor(color: String) {
+        if (!callChangeListener(color)) return
+        this.color = color
+        setSummary(color)
+        persistString(color)
+    }
+}

src/com/xtended/settings/theme/MonetChromaFactorPreferenceController.kt
@@ -1,0 +1,50 @@
+/*
+ * Copyright (C) 2022
+ * SPDX-License-Identifier: Apache-2.0
+*/
+
+package com.xtended.settings.theme
+
+import android.content.Context
+import android.os.UserHandle
+import android.provider.Settings
+
+import androidx.preference.Preference
+
+import com.android.settings.core.BasePreferenceController
+import com.xtended.support.preferences.CustomSeekBarPreference
+
+class MonetChromaFactorPreferenceController(
+    context: Context,
+    key: String,
+) : BasePreferenceController(context, key),
+    Preference.OnPreferenceChangeListener {
+
+    override fun getAvailabilityStatus(): Int = AVAILABLE
+
+    override fun updateState(preference: Preference) {
+        super.updateState(preference)
+        val chromaFactor = Settings.Secure.getFloatForUser(
+            mContext.contentResolver,
+            Settings.Secure.MONET_ENGINE_CHROMA_FACTOR,
+            CHROMA_DEFAULT,
+            UserHandle.USER_CURRENT
+        ) * 100
+        (preference as CustomSeekBarPreference).apply {
+            setValue(chromaFactor.toInt())
+        }
+    }
+
+    override fun onPreferenceChange(preference: Preference, newValue: Any): Boolean {
+        return Settings.Secure.putFloatForUser(
+            mContext.contentResolver,
+            Settings.Secure.MONET_ENGINE_CHROMA_FACTOR,
+            (newValue as Int) / 100f,
+            UserHandle.USER_CURRENT
+        )
+    }
+
+    companion object {
+        private const val CHROMA_DEFAULT = 1f
+    }
+}

src/com/xtended/settings/theme/MonetEngine.kt
@@ -1,0 +1,26 @@
+/*
+ * Copyright (C) 2022
+ * SPDX-License-Identifier: Apache-2.0
+*/
+
+package com.xtended.settings.theme
+
+import com.android.settings.R
+import com.android.settings.search.BaseSearchIndexProvider
+import com.android.settingslib.search.SearchIndexable
+import com.xtended.settings.theme.XDashboardFragment
+
+@SearchIndexable
+class MonetEngine : XDashboardFragment() {
+
+    override protected fun getPreferenceScreenResId() = R.xml.monet
+
+    override protected fun getLogTag() = TAG
+
+    companion object {
+        private const val TAG = "MonetEngine"
+
+        @JvmField
+        val SEARCH_INDEX_DATA_PROVIDER = BaseSearchIndexProvider(R.xml.monet)
+    }
+}

src/com/xtended/settings/theme/SecureSettingsStore.kt
@@ -1,0 +1,51 @@
+/*
+ * Copyright (C) 2022
+ * SPDX-License-Identifier: Apache-2.0
+*/
+
+package com.xtended.settings.theme
+
+import android.content.ContentResolver
+import android.provider.Settings
+
+import androidx.preference.PreferenceDataStore
+
+class SecureSettingsStore(
+    private val contentResolver: ContentResolver
+): PreferenceDataStore() {
+
+    override fun getBoolean(key: String, defValue: Boolean) =
+        getInt(key, if (defValue) 1 else 0) == 1
+
+    override fun getFloat(key: String, defValue: Float) =
+        Settings.Secure.getFloat(contentResolver, key, defValue)
+
+    override fun getInt(key: String, defValue: Int) =
+        Settings.Secure.getInt(contentResolver, key, defValue)
+
+    override fun getLong(key: String, defValue: Long) =
+        Settings.Secure.getLong(contentResolver, key, defValue)
+
+    override fun getString(key: String, defValue: String?): String? =
+        Settings.Secure.getString(contentResolver, key) ?: defValue
+
+    override fun putBoolean(key: String, value: Boolean) {
+        putInt(key, if (value) 1 else 0)
+    }
+
+    override fun putFloat(key: String, value: Float) {
+        Settings.Secure.putFloat(contentResolver, key, value)
+    }
+
+    override fun putInt(key: String, value: Int) {
+        Settings.Secure.putInt(contentResolver, key, value)
+    }
+
+    override fun putLong(key: String, value: Long) {
+        Settings.Secure.putLong(contentResolver, key, value)
+    }
+
+    override fun putString(key: String, value: String?) {
+        Settings.Secure.putString(contentResolver, key, value)
+    }
+}

src/com/xtended/settings/theme/SystemSettingColorPickerPreference.kt
@@ -1,0 +1,18 @@
+/*
+ * Copyright (C) 2022
+ * SPDX-License-Identifier: Apache-2.0
+*/
+
+package com.xtended.settings.theme
+
+import android.content.Context
+import android.util.AttributeSet
+
+public class SystemSettingColorPickerPreference @JvmOverloads constructor(
+    context: Context,
+    attrs: AttributeSet? = null,
+): ColorPickerPreference(context, attrs) {
+    init {
+        setPreferenceDataStore(SystemSettingsStore(context.contentResolver))
+    }
+}

src/com/xtended/settings/theme/SystemSettingsStore.kt
@@ -1,0 +1,51 @@
+/*
+ * Copyright (C) 2022
+ * SPDX-License-Identifier: Apache-2.0
+*/
+
+package com.xtended.settings.theme
+
+import android.content.ContentResolver
+import android.provider.Settings
+
+import androidx.preference.PreferenceDataStore
+
+class SystemSettingsStore(
+    private val contentResolver: ContentResolver
+): PreferenceDataStore() {
+
+    override fun  getBoolean(key: String, defValue: Boolean) =
+        getInt(key, if (defValue) 1 else 0) == 1
+
+    override fun  getFloat(key: String, defValue: Float) =
+        Settings.System.getFloat(contentResolver, key, defValue)
+
+    override fun  getInt(key: String, defValue: Int) =
+        Settings.System.getInt(contentResolver, key, defValue)
+
+    override fun  getLong(key: String, defValue: Long) =
+        Settings.System.getLong(contentResolver, key, defValue)
+
+    override fun  getString(key: String, defValue: String?): String? =
+        Settings.System.getString(contentResolver, key) ?: defValue
+
+    override fun  putBoolean(key: String, value: Boolean) {
+        putInt(key, if (value) 1 else 0)
+    }
+
+    override fun  putFloat(key: String, value: Float) {
+        Settings.System.putFloat(contentResolver, key, value)
+    }
+
+    override fun  putInt(key: String, value: Int) {
+        Settings.System.putInt(contentResolver, key, value)
+    }
+
+    override fun  putLong(key: String, value: Long) {
+        Settings.System.putLong(contentResolver, key, value)
+    }
+
+    override fun  putString(key: String, value: String?) {
+        Settings.System.putString(contentResolver, key, value)
+    }
+}

src/com/xtended/settings/theme/XDashboardFragment.kt
@@ -1,0 +1,34 @@
+/*
+ * Copyright (C) 2022
+ * SPDX-License-Identifier: Apache-2.0
+*/
+
+package com.xtended.settings.theme
+
+import androidx.preference.Preference
+
+import com.android.internal.logging.nano.MetricsProto
+import com.android.settings.dashboard.DashboardFragment
+import com.xtended.settings.theme.ColorPickerFragment
+import com.xtended.settings.theme.ColorPickerPreference
+
+abstract class XDashboardFragment : DashboardFragment() {
+
+    override fun getMetricsCategory(): Int = MetricsProto.MetricsEvent.XTENSIONS
+
+    override fun onDisplayPreferenceDialog(preference: Preference) {
+        if (preference is ColorPickerPreference) {
+            ColorPickerFragment(preference.color).apply {
+                setOnConfirmListener {
+                    preference.setColor(it)
+                }
+            }.show(childFragmentManager, COLOR_PICKER_DIALOG_KEY)
+        } else {
+            super.onDisplayPreferenceDialog(preference)
+        }
+    }
+
+    companion object {
+        const val COLOR_PICKER_DIALOG_KEY = "color_picker_dialog"
+    }
+}